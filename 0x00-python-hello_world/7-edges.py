cdb3168ef1c015fed6059250dc8e7d74bac1819a
khawlaAlbasheer <khawla.elbasheer@gmial.com>
2023-11-30 20:19:49 +0200

    copy-cut-paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-
-
-print(f"First 3 letters: {word[:3]}")
+first3 = word[:3]
+mid = word[1:-1]
+print(f"First 3 letters: {first3}")
 print(f"Last 2 letters: {word[7:]}")
-print(f"Middle word: {word[1:-1]}")
+print(f"Middle word: {mid}")
 
