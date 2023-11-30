f8a121968fe7215cd310ba11e1e0f850f26c8565
khawlaAlbasheer <khawla.elbasheer@gmial.com>
2023-11-30 23:29:59 +0200

    Copy-Cut-Past

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-first3 = word[:3]
-mid = word[1:-1]
-print(f"First 3 letters: {first3}")
-print(f"Last 2 letters: {word[7:]}")
-print(f"Middle word: {mid}")
-
+word_first_3 = word[:4]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
