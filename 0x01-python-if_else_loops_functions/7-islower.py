1a61d66f12d6cdc124469a5d7f35ff37c2e41d9d
khawlaAlbasheer <khawla.elbasheer@gmial.com>
2023-12-03 00:59:52 +0200

    check lowercase

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+def islower(c):
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for i in letters:
+        if c == i:
+            return True
+    return False
