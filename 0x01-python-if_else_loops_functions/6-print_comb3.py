56ed20491bb024fcf6b34ac2aae825df6984a8d3
khawlaAlbasheer <khawla.elbasheer@gmial.com>
2023-12-03 00:49:26 +0200

    possible combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(i+1, 10):
-        print('{}{}'.format(i,j), end="")
+        print('{}{}'.format(i, j), end="")
         if i == 8 and j == 9:
             continue
         print(', ', end="")
