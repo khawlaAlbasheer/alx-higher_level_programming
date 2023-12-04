8ca026951b7d1dc50adba093d70679a9f1d81fd6
khawlaAlbasheer <khawla.elbasheer@gmial.com>
2023-12-04 16:55:13 +0200

    check lowercase

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def islower(c):
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    letters = ['a', 'b', 'c',
+            'd', 'e', 'f', 'g',
+            'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o',
+            'p', 'q', 'r',
+            's', 't', 'u',
+            'v', 'w', 'x',
+            'y', 'z']
     for i in letters:
         if c == i:
             return True
