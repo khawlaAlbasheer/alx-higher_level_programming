d1d94e956b736243f1fbc9be0f852e142da48311
khawlaAlbasheer <khawla.elbasheer@gmial.com>
2024-01-11 20:59:32 +0200

    add two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""add_integer:
+    check the type of givene two value
+    return the sum of the integer values"""
 def add_integer(a, b=98):
 
     """add two integers"""
@@ -10,4 +13,5 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     if type(b) is not int:
         raise TypeError('b must be an integer')
-    return (a + b)
+    else:
+        return (a + b)
