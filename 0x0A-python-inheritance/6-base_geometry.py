50f101a398b30d39df6940536af4d8357ed2d355
khawlaAlbasheer <khawla.elbasheer@gmial.com>
2024-01-15 09:44:43 +0200

    rasie Exception error from a class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry is an empty class"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def area(self):
+        """raise Exception error"""
+        raise Exception('area() is not implemented')
