d264339efc685f2fd64b59cd6c7da0a4b29cd7e0
khawlaAlbasheer <khawla.elbasheer@gmial.com>
2024-01-15 09:38:33 +0200

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""check the object is an instance of a class that inherited"""
 
 
 def inherits_from(obj, a_class):
-    
+    """Return True if the object is instance of a class"""    
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
