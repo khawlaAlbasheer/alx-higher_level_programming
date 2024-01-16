6b22cdc838c833d461306e9e87c7bce36bbaacf4
khawlaAlbasheer <khawla.elbasheer@gmial.com>
2024-01-16 12:48:57 +0200

    print JSON representation of an object (string)

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """usage of json to get str of an obj"""
 
 
+import json
 def to_json_string(my_obj):
     """takes a string object
     returns the JSON representation"""
